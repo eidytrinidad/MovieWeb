88b331efc6f5ce4de6f3b25e0d2fdc551f045ef7
eidytrinidad <eidyjtf@gmail.com>
2020-08-25 19:12:36 -0400

    adding hover to posters

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// const btns = document.querySelectorAll(".MenuBotones button")
+
+// btns.forEach((btn,index)=>{
+    
+//     btn.addEventListener("click",function(e){
+
+//         // if (index===0) {
+//         //     console.log("klk")
+//         // }
+//         // if (index===1) {
+//         //     console.log("wawawa")
+//         // }
+//         // if (index===2) {
+//         //     console.log("Palomo")
+//         // }
+//         // if (index===3) {
+//         //     console.log("Bulto")
+//         // }
+//         // if (index===4) {
+//         //     console.log("Chichigua")
+//         // }
+//         // if (index===5) {
+//         //     console.log("kk")
+//         // }
+//     //     console.log(index)
+      
+//     // e.target.previousElementSibling.classList.remove('active')
+//     // console.log(e.target.parentElement.firstElementChild)
+//     //   e.target.classList.add('active')
+//     //  e.target.nextElementSibling.classList.remove('active')
+//     // })
+    
+// })

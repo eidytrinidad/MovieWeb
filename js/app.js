dfd6f3c0336a841a331192bf2517bcda47f73752
eidytrinidad <eidyjtf@gmail.com>
2020-08-25 23:49:20 -0400

    adding queries

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-// const btns = document.querySelectorAll(".MenuBotones button")
+const btns = document.querySelectorAll(".MenuBotones button")
 
-// btns.forEach((btn,index)=>{
+btns.forEach((btn,index)=>{
     
-//     btn.addEventListener("click",function(e){
+    btn.addEventListener("click",function(e){
 
-//         // if (index===0) {
-//         //     console.log("klk")
-//         // }
-//         // if (index===1) {
-//         //     console.log("wawawa")
-//         // }
-//         // if (index===2) {
-//         //     console.log("Palomo")
-//         // }
-//         // if (index===3) {
-//         //     console.log("Bulto")
-//         // }
-//         // if (index===4) {
-//         //     console.log("Chichigua")
-//         // }
-//         // if (index===5) {
-//         //     console.log("kk")
-//         // }
-//     //     console.log(index)
+        // if (index===0) {
+        //     console.log("klk")
+        // }
+        // if (index===1) {
+        //     console.log("wawawa")
+        // }
+        // if (index===2) {
+        //     console.log("Palomo")
+        // }
+        // if (index===3) {
+        //     console.log("Bulto")
+        // }
+        // if (index===4) {
+        //     console.log("Chichigua")
+        // }
+        // if (index===5) {
+        //     console.log("kk")
+        // }
+    //     console.log(index)
       
-//     // e.target.previousElementSibling.classList.remove('active')
-//     // console.log(e.target.parentElement.firstElementChild)
-//     //   e.target.classList.add('active')
-//     //  e.target.nextElementSibling.classList.remove('active')
-//     // })
+    e.target.previousElementSibling.classList.remove('active')
+    console.log(e.target.parentElement.firstElementChild)
+      e.target.classList.add('active')
+     e.target.nextElementSibling.classList.remove('active')
+    })
     
-// })
+})
 
 
 // const movieInfo = document.querySelector(".movieInfo");

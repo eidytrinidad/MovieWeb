914cf1433a5c10cd89c3435a6eae77ef02fdc7c0
eidytrinidad <eidyjtf@gmail.com>
2020-08-28 00:56:23 -0400

    Getting data from Firebase and loading effect

--- js/app.js
+++ js/app.js
@@ -1,91 +1,238 @@
+// const getTask = () => db.collection("proximamente").get();
+
+// const btn = document.querySelector('button')
+
+// btn.addEventListener('click', async () => {
+
+//   const querySnapshot = await getTask()
+// querySnapshot.forEach(doc => {
+//   console.log(doc.data())
+// });
+
+// })
+
+//COnexion con Firebase
+const db = firebase.firestore()
+
+//Variables del Dom
 const btns = document.querySelectorAll(".MenuBotones button")
 const movieGrid = document.querySelector(".MovieGrid")
-
-console.log(movieGrid)
-
 let contenido = ""
+
+//Queries
+
+//nuevas Peliculas
+const onGetNuevas = (callback) => db.collection("nuevo").onSnapshot(callback)
+
+//Proximas
+const getProximas = () => db.collection("proximamente").get();
+
+//Animes
+const getAnimes = () => db.collection("anime").get();
+
+//Series
+const getSeries = () => db.collection("series").get();
+
+var nuevo = []
+
+window.addEventListener('load', () => {
+
+    onGetNuevas((querySnapshot) => {
+
+        querySnapshot.forEach(doc => {
+            //nuevo.push()
+            const nuevas = doc.data()
+
+        
+            contenido += `<div class="movie">
+            <div class="imagen">
+            <div class="overlay">
+            <div class="info">
+            <p>${nuevas.titulo}</p>
+            <button class="btnVerMas">VER MAS</button>
+            </div>
+            </div>
+            <img src="${nuevas.imagen}" alt="Proximamente1">
+            </div>
+            <div class="texto">
+            <h4 class="titulo">${nuevas.titulo}</h4>
+            <p class="genero">${nuevas.genero}</p>
+            <div class="puntuacion">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <p>${nuevas.puntuacion}</p>
+                        </div>
+            </div>
+            </div>
+            </div>`;
+            movieGrid.innerHTML = contenido
+        })
+    })
+
+})
+
+
 
 //Botones Secciones
 btns.forEach((btn, index) => {
 
-    btn.addEventListener("click", function (e) {
+    btn.addEventListener("click", async function (e) {
         btns.forEach(btn => btn.classList.remove('active'))
         e.target.classList.add("active")
         console.log(index)
 
         if (index === 0) {
             //Nuevo
-            movieGrid.innerHTML = ""
+            contenido = ""
+            movieGrid.innerHTML = "<h3 class='animate__animated animate__flash'><i class='fas fa-spinner'></i> Loading...</h3>"
+            onGetNuevas((querySnapshot) => {
 
+                querySnapshot.forEach(doc => {
+                    //nuevo.push()
+                    const nuevas = doc.data()
+
+                  
+                    contenido += `<div class="movie">
+                    <div class="imagen">
+                    <div class="overlay">
+                    <div class="info">
+                    <p>${nuevas.titulo}</p>
+                    <button class="btnVerMas">VER MAS</button>
+                    </div>
+                    </div>
+                    <img src="${nuevas.imagen}" alt="Proximamente1">
+                    </div>
+                    <div class="texto">
+                    <h4 class="titulo">${nuevas.titulo}</h4>
+                    <p class="genero">${nuevas.genero}</p>
+                    <div class="puntuacion">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <p>${nuevas.puntuacion}</p>
+                    </div>
+                    </div>
+                    </div>
+                    </div>`;
+                    movieGrid.innerHTML = contenido
+                })
+            })
+            movieGrid.innerHTML = contenido
+
+            //contenido+=
 
         } else if (index === 1) {
             //Proximamente
-            contenido=""
-            movieGrid.innerHTML = ""
-            contenido += `<div class="movie">
-            <div class="imagen">
-            <div class="overlay">
-            <div class="info">
-            <p>MULAN</p>
-            <button class="btnVerMas">VER MAS</button>
-            </div>
-            </div>
-            <img src="mulan.jpg" alt="Proximamente1">
-            </div>
-            <div class="texto">
-            <h4 class="titulo">MULAN</h4>
-            <p class="genero">Action, Adventure, Sci-Fi</p>
-            <p><b>PROXIMAMENTE</b></P>
-            </div>
-            </div>
-            </div>`;
+
+            contenido = ""
+            movieGrid.innerHTML = "<h3 class='animate__animated animate__flash'><i class='fas fa-spinner'></i> Loading...</h3>"
+            const querySnapshot = await getProximas()
+
+            querySnapshot.forEach(doc => {
+
+                const proximamente = doc.data()
+               
+
+                contenido += `<div class="movie">
+                <div class="imagen">
+                <div class="overlay">
+                <div class="info">
+                <p>${proximamente.titulo}</p>
+                <button class="btnVerMas">VER MAS</button>
+                </div>
+                </div>
+                <img src="${proximamente.imagen}" alt="Proximamente1">
+                </div>
+                <div class="texto">
+                <h4 class="titulo">MULAN</h4>
+                <p class="genero">${proximamente.genero}</p>
+                <p><b>PROXIMAMENTE</b></P>
+                </div>
+                </div>
+                </div>`;
+            })
+
             movieGrid.innerHTML = contenido
 
         } else if (index === 2) {
             //Anime
-            contenido=""
-            movieGrid.innerHTML = ""
-            contenido += `<div class="movie">
-            <div class="imagen">
-            <div class="overlay">
-            <div class="info">
-            <p>MULAN</p>
-            <button class="btnVerMas">VER MAS</button>
-            </div>
-            </div>
-            <img src="yourName.jpg" alt="Proximamente1">
-            </div>
-            <div class="texto">
-            <h4 class="titulo">YOUR NAME</h4>
-            <p class="genero">Anime, Adventure, Sci-Fi</p>
-            <p><b>PROXIMAMENTE</b></P>
-            </div>
-            </div>
-            </div>`;
+            contenido = ""
+            movieGrid.innerHTML = "<h3 class='animate__animated animate__flash'><i class='fas fa-spinner'></i> Loading...</h3>"
+
+            const querySnapshot = await getAnimes()
+            querySnapshot.forEach(doc => {
+                
+                const Animes= doc.data()
+           
+
+                contenido += `<div class="movie">
+                <div class="imagen">
+                <div class="overlay">
+                <div class="info">
+                <p>${Animes.titulo}</p>
+                <button class="btnVerMas">VER MAS</button>
+                </div>
+                </div>
+                <img src="${Animes.imagen}" alt="Proximamente1">
+                </div>
+                <div class="texto">
+                <h4 class="titulo">${Animes.titulo}</h4>
+                <p class="genero">${Animes.genero}</p>
+                <div class="puntuacion">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <p>${Animes.puntuacion}</p>
+                            </div>
+                </div>
+                </div>
+                </div>`;
+            })
             movieGrid.innerHTML = contenido
 
         } else if (index === 3) {
 
             //Series
-            contenido=""
-            movieGrid.innerHTML = ""
-            contenido += `<div class="movie">
-                    <div class="imagen">
-                    <div class="overlay">
-                    <div class="info">
-                    <p>LUCIFER</p>
-                    <button class="btnVerMas">VER MAS</button>
-                    </div>
-                    </div>
-                    <img src="lucifer.jpg" alt="Proximamente1">
-                    </div>
-                    <div class="texto">
-                    <h4 class="titulo">LUCIFER</h4>
-                    <p class="genero">Action, Sci-Fi</p>
-                    <p><b>PROXIMAMENTE</b></P>
-                    </div>
-                    </div>
-                    </div>`;
+            contenido = ""
+            movieGrid.innerHTML = "<h3 class='animate__animated animate__flash'><i class='fas fa-spinner'></i> Loading...</h3>"
+
+            const querySnapshot = await getSeries()
+            querySnapshot.forEach(doc => {
+                const Series = doc.data()
+               
+
+                contenido += `<div class="movie">
+                <div class="imagen">
+                <div class="overlay">
+                <div class="info">
+                <p>${Series.titulo}</p>
+                <button class="btnVerMas">VER MAS</button>
+                </div>
+                </div>
+                <img src="${Series.imagen}" alt="Proximamente1">
+                </div>
+                <div class="texto">
+                <h4 class="titulo">${Series.titulo}</h4>
+                <p class="genero">${Series.genero}</p>
+                <div class="puntuacion">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <p>${Series.puntuacion}</p>
+                            </div>
+                </div>
+                </div>
+                </div>`;
+            })
             movieGrid.innerHTML = contenido
         } else if (index === 4) {
 
---
+++ js/configFirebase.js
@@ -0,0 +1,23 @@
+// The core Firebase JS SDK is always required and must be listed first 
+
+/*TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries */
+
+
+  // Your web app's Firebase configuration
+
+ 
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+     apiKey: "AIzaSyApL0nXORxkZKBFQ2ptDK8eF09VX2JxKGU",
+     authDomain: "tienda-vidvie.firebaseapp.com",
+     databaseURL: "https://tienda-vidvie.firebaseio.com",
+     projectId: "tienda-vidvie",
+     storageBucket: "tienda-vidvie.appspot.com",
+     messagingSenderId: "464332715076",
+     appId: "1:464332715076:web:89ac8d8b284759a0db7811",
+     measurementId: "G-MDHS7BK3ZD"
+   };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+ 

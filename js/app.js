b1acd42ca4ee35ac0b8fc2f65b8478f65b410403
eidytrinidad <eidyjtf@gmail.com>
2020-09-01 21:58:51 -0400

    Adding Burguer Menu

--- js/app.js
+++ js/app.js
@@ -3,6 +3,28 @@
 //COnexion con Firebase
 const db = firebase.firestore()
 
+
+//Burger
+const btnBurger = document.querySelector(".btnBurger");
+const icon = document.querySelector(".btnBurger i");
+const burgerMenu = document.querySelector(".burgerMenu");
+
+let value = false;
+btnBurger.addEventListener("click", () => {
+
+    console.log(!value)
+    if (!value) {
+        value = true
+        burgerMenu.style.display = "flex";
+        icon.classList.add("fa-times");
+    } else {
+        value = false;
+        burgerMenu.style.display = "none";
+        icon.classList.remove("fa-times")
+    }
+})
+
+
 //Variables del Dom
 const btns = document.querySelectorAll(".MenuBotones button")
 const movieGrid = document.querySelector(".MovieGrid")
